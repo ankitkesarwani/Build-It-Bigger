fffcef4d3371d8e9bb780f3535b3f6a708255c7c
ankitkesarwani <kesarwani.ankit48@gmail.com>
2018-12-09 12:46:30 +0530

    final commit

---
+++ myjokes-android/src/main/java/com/developers/myjokes_android/JokeActivity.java
@@ -0,0 +1,37 @@
+package com.developers.myjokes_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String JOKE_KEY = "jokes";
+    TextView jokeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        setContentView(R.layout.activity_joke);
+        jokeText = (TextView) findViewById(R.id.joke_text_view);
+        String key = getIntent().getStringExtra(JOKE_KEY);
+        jokeText.setText(key);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        //Back Button to navigate back to the details screen
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
+    }
+
+}
